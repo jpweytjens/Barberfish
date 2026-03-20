d713c28564083d58985643f14391ea5933112afd
Johannes Weytjens <github@peregrinus.be>
2026-03-20 17:50:04 +0100

    chore: bump version to 1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.jpweytjens.barberfish"
         minSdk = 23
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
     }
 
     signingConfigs {
